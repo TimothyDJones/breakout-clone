c58a437c9f225dc9c7d56b1bfafd4046925428f9
Tim Jones <tdjones74021@yahoo.com>
2021-01-13 11:35:26 -0600

    Complete implementation of basic functionality to display game window.

--- game/__init__.py
+++ game/__init__.py
@@ -1,9 +1,26 @@
+import pygame
+import sys
+
+# TODO: Load game assets/components.
+from game.constants import Constants
+
 class Game():
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(size=(Constants.screen_width, Constants.screen_height))
+        self.clock = pygame.time.Clock()
+
+        self.bg_color = pygame.Color("black")
+        self.font = pygame.font.Font("assets/Kenney_Future.ttf", 16)
+
     def handle_events(self):
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
 
     def update(self):
         pass
 
     def draw(self):
-        pass
+        self.screen.fill(self.bg_color)
